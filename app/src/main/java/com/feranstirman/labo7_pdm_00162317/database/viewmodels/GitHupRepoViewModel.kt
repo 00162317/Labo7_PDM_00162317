c9e5c22ac0429233327a9dfa393350c9f2f86899
Roberto <00162317@uca.edu.sv>
2019-05-17 14:53:35 -0600

    Dispatchers, despachan un hilo

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/viewmodels/GitHupRepoViewModel.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/viewmodels/GitHupRepoViewModel.kt
@@ -2,8 +2,13 @@ package com.feranstirman.labo7_pdm_00162317.database.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.feranstirman.labo7_pdm_00162317.database.RoomDB
+import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
 import com.feranstirman.labo7_pdm_00162317.database.repositories.GitHubRepoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.lang.Appendable
 
 class GitHupRepoViewModel(app:Application):AndroidViewModel(app){
@@ -12,8 +17,13 @@ class GitHupRepoViewModel(app:Application):AndroidViewModel(app){
 
     init {
         val repoDao=RoomDB.getInstance(app).repoDAO()
-        repository=GitHubRepoRepository(repoDao)
+        repository= GitHubRepoRepository(repoDao)
     }
 
+    fun getAll():LiveData<List<GitHubRepo>> = repository.getAll()
+
+    fun insert(repo: GitHubRepo)=viewModelScope.launch (Dispatchers.IO){
+
+    }
 
 }
