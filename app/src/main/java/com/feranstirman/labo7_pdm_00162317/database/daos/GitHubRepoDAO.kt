6039fc870531d6e9269c462fd60be2e8c6dd9077
Roberto <00162317@uca.edu.sv>
2019-05-17 14:27:14 -0600

    Para mostrarlos

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
@@ -1,8 +1,11 @@
 package com.feranstirman.labo7_pdm_00162317.database.daos
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
 
 @Dao
 interface GitHubRepoDAO{
 
+    fun getAll():LiveData<List<GitHubRepo>>
 }
