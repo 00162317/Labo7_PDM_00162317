5f3bbc97ea92c77b84bc3cfa873829bc0dceca6b
Roberto <00162317@uca.edu.sv>
2019-05-17 14:23:52 -0600

    ColumnInfo nombra como el alias para realizar la Querys se ocupa "s_name"

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/entities/GitHubRepo.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/entities/GitHubRepo.kt
@@ -1,10 +1,12 @@
 package com.feranstirman.labo7_pdm_00162317.database.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "repos")
 data class GitHubRepo(
+    @ColumnInfo(name = "s_name")
     val name:String
 ){
     @PrimaryKey(autoGenerate = true)
