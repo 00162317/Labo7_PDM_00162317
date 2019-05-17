06cab7ebd12ba3589a61f55efe95e1615bf2c9fd
Roberto <00162317@uca.edu.sv>
2019-05-17 14:38:01 -0600

    Creando la base de datos. Ya vinculamos la DAO y la Entity

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/RoomDB.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/RoomDB.kt
@@ -1,10 +1,33 @@
 package com.feranstirman.labo7_pdm_00162317.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.feranstirman.labo7_pdm_00162317.database.daos.GitHubRepoDAO
 import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
 
 @Database(entities = [GitHubRepo::class],version = 1,exportSchema = false)
 public abstract class RoomDB:RoomDatabase(){
+
+    abstract fun repoDAO():GitHubRepoDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE:RoomDB?=null
+
+        fun getInstance(context: Context):RoomDB{
+            val temInstance = INSTANCE
+            if(temInstance!=null){
+                return temInstance
+            }
+            synchronized(this){
+                val instance= Room.databaseBuilder(context,RoomDB::class.java,"Repo_DB").build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 
 }
