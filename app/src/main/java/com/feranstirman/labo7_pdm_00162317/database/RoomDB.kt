6f88bdbacf32a9dd64dd052e01a3dc74078163f0
Roberto <00162317@uca.edu.sv>
2019-05-17 14:33:57 -0600

    Declarando las entidades con la notacion y obteniendo del modelo GIthubREpo

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/RoomDB.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/RoomDB.kt
@@ -1,2 +1,10 @@
 package com.feranstirman.labo7_pdm_00162317.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
+
+@Database(entities = [GitHubRepo::class],version = 1,exportSchema = false)
+public abstract class RoomDB:RoomDatabase(){
+
+}
