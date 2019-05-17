c4ad7d6d74a693e0637129bb81cf1923b1de09cf
Roberto <00162317@uca.edu.sv>
2019-05-17 14:30:08 -0600

    Suspend indica que solo se puede ocupar en la corrutina

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
@@ -3,6 +3,7 @@ package com.feranstirman.labo7_pdm_00162317.database.daos
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
 
@@ -12,8 +13,8 @@ interface GitHubRepoDAO{
     @Query("SELECT * FROM repos")
     fun getAll():LiveData<List<GitHubRepo>>
 
-    @Insert
-    fun insert(repo:GitHubRepo)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(repo:GitHubRepo)
 
     @Query("DELETE FROM repos")
     fun nukeTable()
