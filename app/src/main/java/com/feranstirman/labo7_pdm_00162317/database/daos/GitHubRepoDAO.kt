e2bf9638e1f3cf0aa58c00195ffbc473ab861a7c
Roberto <00162317@uca.edu.sv>
2019-05-17 14:29:04 -0600

    Notaciones para hacer las consultas(?)

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/daos/GitHubRepoDAO.kt
@@ -2,14 +2,19 @@ package com.feranstirman.labo7_pdm_00162317.database.daos
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import com.feranstirman.labo7_pdm_00162317.database.entities.GitHubRepo
 
 @Dao
 interface GitHubRepoDAO{
 
+    @Query("SELECT * FROM repos")
     fun getAll():LiveData<List<GitHubRepo>>
 
+    @Insert
     fun insert(repo:GitHubRepo)
 
+    @Query("DELETE FROM repos")
     fun nukeTable()
 }
