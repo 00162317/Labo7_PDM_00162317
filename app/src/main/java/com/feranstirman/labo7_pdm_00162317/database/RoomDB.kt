220cf0039def24640a6932564e26efabbdb03415
Roberto <00162317@uca.edu.sv>
2019-05-17 14:20:28 -0600

    Hicimos entity

---
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/RoomDB.kt
@@ -0,0 +1,2 @@
+package com.feranstirman.labo7_pdm_00162317.database
+
---
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/entities/GitHubRepo.kt
@@ -0,0 +1,6 @@
+package com.feranstirman.labo7_pdm_00162317.database.entities
+
+import androidx.room.Entity
+
+@Entity(tableName = "repos")
+data class GitHubRepo()
