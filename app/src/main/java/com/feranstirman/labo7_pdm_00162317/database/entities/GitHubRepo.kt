d4af02d8e2ef5425418cb24f640391d73e3a56b0
Roberto <00162317@uca.edu.sv>
2019-05-17 14:22:05 -0600

    El id vamos haciendo que se autogenere. Este sera nuestra llave primaria

--- app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/entities/GitHubRepo.kt
+++ app/src/main/java/com/feranstirman/labo7_pdm_00162317/database/entities/GitHubRepo.kt
@@ -1,6 +1,12 @@
 package com.feranstirman.labo7_pdm_00162317.database.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "repos")
-data class GitHubRepo()
+data class GitHubRepo(
+    val name:String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id:Long=0
+}
